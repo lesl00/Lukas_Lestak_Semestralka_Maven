9dd7ef8954709f53cc710f97eb2e911b476beccb
lukaslestak <lesl00@vse.cz>
2020-01-06 19:54:03 +0100

    TASK SUITe

--- src/test/java/cz/vse/selenium/TS_TASK.java
+++ src/test/java/cz/vse/selenium/TS_TASK.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ import java.io.IOException;
 /**
  * Unit test for simple App.
  */
-public class TS_TASK {
+public class TS_PROJECT {
     private ChromeDriver driver;
     private String url="https://digitalnizena.cz/rukovoditel/index.php?module=users/login";
 
@@ -40,43 +41,77 @@ public class TS_TASK {
 //        driver.close();
     }
 
-    @Test
-    public void login_first_test() {
+    public void Prihlasenie(){
         driver.get(url);
         WebElement searchInput = driver.findElement(By.name("username"));
         searchInput.sendKeys("rukovoditel");
         searchInput = driver.findElement(By.name("password"));
         searchInput.sendKeys("vse456ru");
         searchInput.sendKeys(Keys.ENTER);
-        Assert.assertTrue(driver.getTitle().startsWith("Rukovoditel | Dashboard"));
-        driver.quit();
+    }
+
+
+    public void NewProject(){
+        driver.findElement(By.cssSelector("li:nth-child(4) .title:nth-child(2)")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        //Name: xname
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_158")));
+        WebElement searchInput = driver.findElement(By.id("fields_158"));
+        searchInput.sendKeys("vism00");
+        //status
+        driver.findElement(By.id("fields_157"));
+        Select select = new Select(driver.findElement(By.id("fields_157")));
+        select.selectByIndex(0);
+        //priority: high
+        driver.findElement(By.id("fields_156"));
+        select = new Select(driver.findElement(By.id("fields_156")));
+        select.selectByIndex(1);
+        //Status: new
+        //Assert.assertTrue(driver.findElement(By.id("fields_157")).isDisplayed());
+        driver.findElement(By.id("fields_159")).click();
+        driver.findElement(By.cssSelector("td[class='active day']")).click();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
     }
 
     @Test
-    public void login_second_test() {
-        driver.get(url);
-        WebElement searchInput = driver.findElement(By.name("username"));
-        searchInput.sendKeys("testNeprejde");
-        searchInput = driver.findElement(By.name("password"));
-        searchInput.sendKeys("vse456ru");
-        searchInput.sendKeys(Keys.ENTER);
-        Assert.assertTrue(!driver.getTitle().startsWith("Rukovoditel | Dashboard"));
-        driver.quit();
+    public void new_task_sixth_test() {
+        Prihlasenie();
+        NewProject();
+
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_168")));
+        //name
+        WebElement searchInput = driver.findElement(By.id("fields_168"));
+        searchInput.sendKeys("vism00_task");
+
+        //type
+        driver.findElement(By.id("fields_167"));
+        Select select = new Select(driver.findElement(By.id("fields_167")));
+        select.selectByIndex(1);
+
+        //status
+        driver.findElement(By.id("fields_169"));
+        select = new Select(driver.findElement(By.id("fields_169")));
+        select.selectByIndex(0);
+
+        //priority
+        driver.findElement(By.id("fields_170"));
+        select = new Select(driver.findElement(By.id("fields_170")));
+        select.selectByIndex(2);
+
+        //description
+        driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+        driver.findElement(By.tagName("body")).sendKeys("Description test");
+        driver.switchTo().defaultContent();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+
+
+
+
+
+
     }
 
-    @Test
-    public void logout_third_test() {
-        driver.get(url);
-        WebElement searchInput = driver.findElement(By.name("username"));
-        searchInput.sendKeys("rukovoditel");
-        searchInput = driver.findElement(By.name("password"));
-        searchInput.sendKeys("vse456ru");
-        searchInput.sendKeys(Keys.ENTER);
-        driver.findElement(By.cssSelector(".fa-angle-down")).click();
-        driver.findElement(By.cssSelector(".fa-angle-down")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 2);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Logoff")));
-        driver.findElement(By.linkText("Logoff")).click();
-        driver.quit();
-    }
-}
+}}
